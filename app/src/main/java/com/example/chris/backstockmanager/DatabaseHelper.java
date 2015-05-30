aeff62b577f2276bbf09487f40ae1a0b01807bdf
backstockmanager <backstockmanager@gmail.com>
2015-05-30 10:05:23 -0500

    db helper and wrapper done, not tested or implemented

---
+++ app/src/main/java/com/example/chris/backstockmanager/DatabaseHelper.java
@@ -0,0 +1,63 @@
+package com.example.chris.backstockmanager;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.util.Log;
+
+/**
+ * Created by Chris on 5/28/2015.
+ */
+
+public class DatabaseHelper extends SQLiteOpenHelper {
+
+    private static final String DATABASE_NAME = "backstockdb";
+
+    private static final int DATABASE_VERSION = 1;
+
+    // Database creation sql statement
+    private static final String DATABASE_CREATE_USERS =
+           "create table if not exists users"   +
+           "( name text primary key not null,"  +
+           "password text not null,"            +
+           "security int default 1,"            +
+           "theme text);";
+
+    private static final String DATABASE_CREATE_TOTES =
+           "create table if not exists totes" +
+           "( label text not null,"           +
+           "color text not null,"             +
+           "size text not null,"              +
+           "d-filled text,"                   +
+           "sex text,"                        +
+           "category text,"                   +
+           "subcategory text,"                +
+           "season text,"                     +
+           "hung integer,"                    +
+           "sensor integer,"                  +
+           "offsite integer,"                 +
+           "location text,"                   +
+           "user text,"                       +
+           "primary key (label,color,size));";
+
+    public DatabaseHelper(Context context) {
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+    }
+
+    // Method is called during creation of the database
+    @Override
+    public void onCreate(SQLiteDatabase database) {
+        database.execSQL(DATABASE_CREATE_USERS);
+        database.execSQL(DATABASE_CREATE_TOTES);
+    }
+
+    // Method is called during an upgrade of the database,
+    @Override
+    public void onUpgrade(SQLiteDatabase database,int oldVersion,int newVersion){
+        Log.w(DatabaseHelper.class.getName(),
+                "Upgrading database from version " + oldVersion + " to "
+                        + newVersion + ", which will destroy all old data");
+        //database.execSQL("DROP TABLE IF EXISTS MyEmployees");
+        onCreate(database);
+    }
+}
---
+++ app/src/main/java/com/example/chris/backstockmanager/dbwrapper.java
@@ -0,0 +1,86 @@
+package com.example.chris.backstockmanager;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+
+/**
+ * Created by Chris on 5/30/2015.
+ */
+public class dbwrapper
+{
+
+    public final static String USERS_TABLE     = "users";    // name of table
+    public final static String TOTES_TABLE     = "totes";    // name of table
+
+    public final static String USERS_NAME       = "name";        // name of user
+    public final static String USERS_PASSWORD   = "password";    // password of user
+    public final static String USERS_SECURITY   = "security";    // security of user
+    public final static String USERS_THEME      = "theme";       // theme of user
+    public final static String TOTE_LABEL       = "label";       // label of tote
+    public final static String TOTE_COLOR       = "color";       // color of tote
+    public final static String TOTE_SIZE        = "size";        // size of tote
+    public final static String TOTE_DATE        = "d-filled";    // date of tote
+    public final static String TOTE_SEX         = "sex";         // date of tote
+    public final static String TOTE_CATEGORY    = "category";    // date of tote
+    public final static String TOTE_SUBCATEGORY = "subcategory"; // date of tote
+    public final static String TOTE_SEASON      = "season";      // date of tote
+    public final static String TOTE_HUNG        = "hung";        // date of tote
+    public final static String TOTE_SENSOR      = "sensor";      // date of tote
+    public final static String TOTE_OFFSITE     = "offsite";     // date of tote
+    public final static String TOTE_LOCATION    = "location";    // date of tote
+    public final static String TOTE_USER        = "user";    // date of tote
+
+
+    private DatabaseHelper dbHelper;
+    private SQLiteDatabase database;
+
+    public dbwrapper(Context context)
+    {
+        dbHelper = new DatabaseHelper(context);
+        database = dbHelper.getWritableDatabase();
+    }
+
+    public long createUsersRecord(String name, String password, String security, String theme)
+    {
+        ContentValues values = new ContentValues();
+        values.put(USERS_NAME, name);
+        values.put(USERS_PASSWORD, password);
+        values.put(USERS_SECURITY, security);
+        values.put(USERS_THEME, theme);
+        return database.insert(USERS_TABLE, null, values);
+    }
+
+    public long cresteTotesRecord(String label, String color, String size, String date,
+                                  String sex, String category, String subcategory,
+                                  String season, String hung, String sensor, String offsite,
+                                  String location, String user)
+    {
+        ContentValues values = new ContentValues();
+        values.put(TOTE_LABEL, label);
+        values.put(TOTE_COLOR, color);
+        values.put(TOTE_SIZE, size);
+        values.put(TOTE_DATE, date);
+        values.put(TOTE_SEX, sex);
+        values.put(TOTE_CATEGORY, category);
+        values.put(TOTE_SUBCATEGORY, subcategory);
+        values.put(TOTE_SEASON, season);
+        values.put(TOTE_HUNG, hung);
+        values.put(TOTE_SENSOR, sensor);
+        values.put(TOTE_OFFSITE, offsite);
+        values.put(TOTE_LOCATION, location);
+        values.put(TOTE_USER, user);
+        return database.insert(TOTES_TABLE, null, values);
+    }
+
+    /*public Cursor selectRecords() {
+        String[] cols = new String[] {EMP_ID, EMP_NAME};
+        Cursor mCursor = database.query(true, EMP_TABLE,cols,null
+                , null, null, null, null, null);
+        if (mCursor != null) {
+            mCursor.moveToFirst();
+        }
+        return mCursor; // iterate to get each value.
+    }*/
+}
